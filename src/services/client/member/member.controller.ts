20ebbd0cb495c12f9a5f0b525a18d8a2cceaa5e6
Henri Susanto <henri@member.id>
2020-04-26 09:36:12 +0700

    update member profile

--- src/services/client/member/member.controller.ts
+++ src/services/client/member/member.controller.ts
@@ -4,6 +4,7 @@ import { Http2ServerResponse } from 'http2'
 import { ClientEnrollNewMember } from '../../../domain/Core/Member/UseCase/client.enrollnewmember.usecase'
 import { ClientGetMemberList } from '../../../domain/Core/Member/UseCase/client.getmemberlist.usecase'
 import { ClientGetMemberProfile } from '../../../domain/Core/Member/UseCase/client.getmemberprofile.usecase'
+import { ClientUpdateMemberProfile } from '../../../domain/Core/Member/UseCase/client.updatememberprofile.usecase'
 import { MemberRepository } from '../../../repository/member.repository'
 
 @Controller({ prefix: 'api/member' })
@@ -54,6 +55,26 @@ export class MemberController {
 	  	const memberRepo = new MemberRepository()
 	  	const useCase = new ClientGetMemberProfile(memberRepo)
 	  	const result = await useCase.execute(id)
+	    reply.sendOk(result)
+  	} catch (error) {
+  		reply.sendError(error)
+  	}
+  }
+
+  @Post({ url: '/profile/:id' })
+  async updateProfile (request: FastifyRequest, reply: FastifyReply<Http2ServerResponse>): Promise<void> {
+  	try {
+	  	const payload = JSON.parse(request.body)
+	  	const memberRepo = new MemberRepository()
+	  	const useCase = new ClientUpdateMemberProfile(memberRepo)
+	  	const result = await useCase.execute(
+	  		parseInt (request.params.id),
+	  		payload.name,
+	  		payload.email,
+	  		payload.phone,
+	  		payload.register_date,
+	  		payload.date_of_birth
+	  	)
 	    reply.sendOk(result)
   	} catch (error) {
   		reply.sendError(error)
