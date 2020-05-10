77198b0e10d6464d292022175dd808a8475899f6
Henri Susanto <henri@member.id>
2020-05-10 12:25:39 +0700

    client deduct member point

--- src/services/client/point/point.controller.ts
+++ src/services/client/point/point.controller.ts
@@ -3,6 +3,7 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 import { Http2ServerResponse } from 'http2'
 import { ClientUpdatePointNameUseCase } from '../../../domain/LoyaltyCore/UseCase/Point/client.updatepointname.usecase'
 import { ClientAddMemberPointUseCase } from '../../../domain/LoyaltyCore/UseCase/Point/client.addmemberpoint.usecase'
+import { ClientDeductMemberPointUseCase } from '../../../domain/LoyaltyCore/UseCase/Point/client.deductmemberpoint.usecase'
 import { ConfigRepository } from '../../../repositories/config.repository'
 import { PointRepository } from '../../../repositories/point.repository'
 
@@ -28,6 +29,19 @@ export class PointController {
       const { Member, YTDPoint, LifetimePoint, Remarks } = JSON.parse(request.body)
       let repo = new PointRepository ()
       let useCase = new ClientAddMemberPointUseCase (repo)
+      let Id = await useCase.execute ( Member, YTDPoint, LifetimePoint, Remarks )
+      reply.sendOk({ Id })
+    } catch (error) {
+      reply.sendError(error)
+    }
+  }
+
+  @Post({ url: '/deduct' })
+  async deduct (request: FastifyRequest, reply: FastifyReply<Http2ServerResponse>): Promise<void> {
+    try {
+      const { Member, YTDPoint, LifetimePoint, Remarks } = JSON.parse(request.body)
+      let repo = new PointRepository ()
+      let useCase = new ClientDeductMemberPointUseCase (repo)
       let Id = await useCase.execute ( Member, YTDPoint, LifetimePoint, Remarks )
       reply.sendOk({ Id })
     } catch (error) {
