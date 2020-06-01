35161b669865d1a061edf329ae7b53bc216f28d1
Henri Susanto <henri@member.id>
2020-06-02 05:25:12 +0700

    better point calculation

--- src/repositories/point.repository.ts
+++ src/repositories/point.repository.ts
@@ -14,37 +14,20 @@ export class PointRepository implements PointRepositoryInterface {
       this.repo = this.conn.getRepository(Point)
   }
 
-	public async SummarizeYTDPointByMember (Member: number): Promise <number> {
-		let { sum } = await this.repo
-		  .createQueryBuilder('point')
-		  .select('SUM(YTDAmount)', 'sum')
-		  .where ('Member', Member)
-		  .getRawOne()
-		return sum || 0
-	}
+	public async findPointToUse (criteria): Promise <PointEntity []> {
+		var query = this.repo
+			.createQueryBuilder('point')
+			.select('*')
+		  .where (1)
 
-	public async SummarizeLifetimePointByMember (Member: number): Promise <number> {
-		let { sum } = await this.repo
-		  .createQueryBuilder('point')
-		  .select('SUM(LifetimeAmount)', 'sum')
-		  .where ('Member', Member)
-		  .getRawOne()
-		return sum || 0
-	}
+		 Object.keys(criteria).forEach(field => {
+		 	query.andWhere(`${field} ${criteria[field]}`)
+		 })
 
-	public async findLifetimeRemainingGreaterThan0SortByTime (Member: number): Promise <PointEntity []> {
-		var domains: PointEntity [] = []
-		const found = await this.repo.find ({
-			Member,
-			LifetimeRemaining: MoreThan(0),
-	    order: {
-        Time: 'ASC',
-	    }
-		})
-		found.forEach (record => {
-			domains.push (this.toDomain (record))
-		})
-		return domains
+		 let result = await query.getRawMany ()
+		 return result.map(record => {
+		 	return this.toDomain (record)
+		 })
 	}
 
 	public async bulkSave (points: PointEntity []): Promise <number[]> {
